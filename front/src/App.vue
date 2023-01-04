0720a87455a01a3090c98f2a9f15534596a6880c
otokichi3 <tomorrow.is.another.day1829@gmail.com>
2023-01-04 17:41:27 +0900

    replace front

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,36 +1,17 @@
 <template>
-  <div v-for="match in info" :key="match.id">
-    <table class="court" style="margin: 15px;">
-      <tr>
-        <td>{{ match.pair1.player1.name }}</td>
-        <td>{{ match.pair1.player2.name }}</td>
-      </tr>
-      <tr>
-        <td>vs</td>
-      </tr>
-      <tr>
-        <td>{{ match.pair2.player1.name }}</td>
-        <td>{{ match.pair2.player2.name }}</td>
-      </tr>
-    </table>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      info: null
-    }
-  },
-  mounted() {
-    axios
-      .get('http://localhost:8000')
-      .then(res => (this.info = res.data.matchs))
-      .catch((error) => console.log(error));
+  components: {
+    HelloWorld
   }
 }
 </script>
@@ -40,12 +21,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-table,
-th,
-td {
-  border: 1px solid;
 }
 </style>
