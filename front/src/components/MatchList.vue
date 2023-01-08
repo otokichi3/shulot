01cdee399209eefa1c53fcb50ff471676934bb42
otokichi3 <tomorrow.is.another.day1829@gmail.com>
2023-01-08 15:56:50 +0900

    sqlとfront修正

--- front/src/components/MatchList.vue
+++ front/src/components/MatchList.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container>
 		<v-row class="mt-6 text-center">
-			<v-col cols="6" v-for="(match, index) in info" :key="match.id">
+			<v-col cols="6" v-for="(match, index) in matchs" :key="match.id">
 				<v-card class="light-green lighten-5 mx-auto" cols="12" max-width="400">
 					<v-card-title class="subtitle-1 justify-center pt-1 pb-1">
 						{{ index + 1 }}コート
@@ -39,15 +39,23 @@
 		</v-row>
 		<v-row>
 			<v-col class="text-center">
-				<v-btn color="light-green" :disabled="isMatchFixed" @click="getMatchList">
+				<v-btn color="light-green" min-width="150" :disabled="isMatchFixed || loading" :loading="loading"
+					@click="loader = 'loading'; getMatchList();">
 					試合を組む
 				</v-btn>
 			</v-col>
 		</v-row>
 		<v-row>
 			<v-col class="text-center">
-				<v-btn color="blue-grey lighten-4" :disabled="isMatchFixed" @click="confirmMatch">
+				<v-btn color="blue-grey lighten-4" min-width="150" :disabled="isMatchFixed" @click="confirmMatch()">
 					試合確定
+				</v-btn>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col class="text-center">
+				<v-btn color="blue lighten-4" min-width="150" :disabled="isGameEnd || loading2" :loading="loading2" @click="loader = 'loading2'; nextMatch()">
+					次の試合
 				</v-btn>
 			</v-col>
 		</v-row>
@@ -61,7 +69,8 @@ export default {
 	name: 'MatchList',
 	data: () => ({
 		isMatchFixed: false,
-		info: [
+		isGameEnd: true,
+		init_match: [
 			{
 				pair1: { player1: { name: "　", level: 0, sex: 0, }, player2: { name: "　", level: 0, sex: 0, }, },
 				pair2: { player1: { name: "　", level: 0, sex: 0, }, player2: { name: "　", level: 0, sex: 0, }, }
@@ -79,26 +88,42 @@ export default {
 				pair2: { player1: { name: "　", level: 0, sex: 0, }, player2: { name: "　", level: 0, sex: 0, }, }
 			},
 		],
+		matchs: [],
 		create_result: null,
 		backend_host: 'http://localhost:8000',
 		pair_id: [],
+		loader: null,
+		loading: false,
+		loading2: false,
 	}),
+	watch: {
+		loader() {
+			const l = this.loader
+			this[l] = !this[l]
+
+			setTimeout(() => (this[l] = false), 1000)
+
+			this.loader = null
+		},
+	},
+	mounted() {
+		this.matchs = this.init_match;
+	},
 	methods: {
 		confirmMatch() {
 			// 試合確定フラグを立てる
-			this.isMatchFree = false;
 			this.isMatchFixed = true;
 
 			// 確定したペア、組み合わせを登録する
-			for (let key in this.info) {
+			for (let key in this.matchs) {
 				let pair_id = [];
 				let pair1 = {
-					'player1_id': this.info[key].pair1.player1.id,
-					'player2_id': this.info[key].pair1.player2.id,
+					'player1_id': this.matchs[key].pair1.player1.id,
+					'player2_id': this.matchs[key].pair1.player2.id,
 				}
 				let pair2 = {
-					'player1_id': this.info[key].pair2.player1.id,
-					'player2_id': this.info[key].pair2.player2.id,
+					'player1_id': this.matchs[key].pair2.player1.id,
+					'player2_id': this.matchs[key].pair2.player2.id,
 				}
 				Promise.resolve()
 					// 組み合わせごとのペアをテーブルに登録する
@@ -112,14 +137,20 @@ export default {
 					.catch(e => { console.log(e) })
 					.finally(() => { pair_id = [] })
 			}
+			// 次の試合ボタンを活性化する
+			this.isGameEnd = false;
 		},
 		getMatchList() {
 			axios
 				.get(this.backend_host)
-				.then(res => (this.info = res.data.matchs))
+				.then(res => {this.matchs = res.data.matchs; console.log(this.matchs)})
 				.catch((error) => console.log(error));
-			this.isMatchConfirmed = false;
-		}
+			this.isMatchFixed = false;
+		},
+		nextMatch() {
+			this.isGameEnd = true;
+			this.getMatchList();
+		},
 	}
 }
 </script>
